cb841a89f0187822743930f2969a78e84534366e
zhuqianglu <rxontdkd@gmail.com>
2023-06-05 23:07:21 +1000

    working demo

--- pytorch_mask_rcnn/__init__.py
+++ pytorch_mask_rcnn/__init__.py
@@ -4,7 +4,4 @@ from .engine import train_one_epoch, evaluate
 from .utils import *
 from .gpu import *
 
-try:
-    from .visualizer import *
-except ImportError:
-    pass
+from .visualizer import *
